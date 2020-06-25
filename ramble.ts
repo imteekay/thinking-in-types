e32f84d96be6afb67f562568c64f8cac3731db25
TK <leandrotk100@gmail.com>
2020-06-25 09:07:20 -0300

    Shape example

--- ramble.ts
+++ ramble.ts
@@ -1,20 +1,59 @@
 let city = 'new york city';
 console.log(city.toUppercase());
+console.log(city.toUpperCase());
 
 // Property 'toUppercase' does not exist on type
 // 'string'.Did you mean 'toUpperCase' ?
 
 // ---------------------------------------------------------
 
-const states = [
+const people = [
   { name: 'TK', age: 24 },
   { name: 'Kaio', age: 12 },
   { name: 'Kazumi', age: 31 },
 ];
 
-for (const state of states) {
-  console.log(state.agi);
+for (const person of people) {
+  console.log(person.agi);
+  // Property 'agi' does not exist on type '{ name: string; age: number; }'
+}
+
+type People = {
+  name: string;
+  age: number;
+}
+
+const people: People = [
+  { name: 'TK', age: 24 },
+  { name: 'Kaio', age: 12 },
+  { name: 'Kazumi', age: 31 },
+];
+
+for (const person of people) {
+  console.log(person.agi);
   // Property 'agi' does not exist on type '{ name: string; age: number; }'
 }
 
 // ---------------------------------------------------------
+
+interface Square {
+  width: number;
+}
+
+interface Rectangle extends Square {
+  height: number;
+}
+
+type Shape = Square | Rectangle;
+
+function calculateArea(shape: Shape) {
+  if (shape instanceof Rectangle) {
+    // ~~~~~~~~~ 'Rectangle' only refers to a type,
+    //           but is being used as a value here
+    return shape.width * shape.height;
+    // ~~~~~~ Property 'height' does not exist
+    //
+  } else {
+    return shape.width * shape.width;
+  }
+}

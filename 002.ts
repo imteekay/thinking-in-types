0d0d3ca81074d4f2f46cf0a30d7c6af9384db2ad
TK <leandrotk100@gmail.com>
2020-06-28 21:58:53 -0300

    Thinking in types 002

---
+++ 002.ts
@@ -0,0 +1,26 @@
+const people = [
+  { name: 'TK', age: 24 },
+  { name: 'Kaio', age: 12 },
+  { name: 'Kazumi', age: 31 },
+];
+
+for (const person of people) {
+  console.log(person.agi);
+  // Property 'agi' does not exist on type '{ name: string; age: number; }'
+}
+
+type People = {
+  name: string;
+  age: number;
+}
+
+const people: People = [
+  { name: 'TK', age: 24 },
+  { name: 'Kaio', age: 12 },
+  { name: 'Kazumi', age: 31 },
+];
+
+for (const person of people) {
+  console.log(person.agi);
+  // Property 'agi' does not exist on type '{ name: string; age: number; }'
+}

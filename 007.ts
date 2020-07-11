2ce8c673ed8a10d758c8fa9c09b2789784d67cfc
TK <leandrotk100@gmail.com>
2020-07-11 12:52:32 -0300

    007: immutability in compile-time

---
+++ 007.ts
@@ -0,0 +1,48 @@
+// Using `as const` to provide immutability in compile-time
+const author = {
+  name: 'Walter Isaacson',
+  email: 'walter.isaacson@mail.com',
+  books: [
+    {
+      title: 'Leonardo Da Vinci',
+      price: 50.00,
+    }
+  ]
+} as const;
+
+author.name = 'TK';
+author.books.push({
+  title: 'Steve Jobs',
+  price: 30
+});
+
+// Using Readonly to provide immutability in compile-time
+type Book = {
+  title: string;
+  price: number;
+};
+
+type Author = Readonly<{
+  name: string;
+  email: string;
+  books: ReadonlyArray<Book>;
+}>;
+
+const author: Author = {
+  name: 'Walter Isaacson',
+  email: 'walter.isaacson@mail.com',
+  books: [
+    {
+      title: 'Leonardo Da Vinci',
+      price: 50.00,
+    }
+  ]
+};
+
+const book: Book = {
+  title: 'Steve Jobs',
+  price: 30
+};
+
+author.name = 'TK';
+author.books.push(book);

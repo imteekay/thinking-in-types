bdda998afa820ec6e1852edda08a116ec7e34b4d
TK <leandrotk100@gmail.com>
2020-07-15 21:36:57 -0300

    007: Value space vs Type space

--- 007-value-space-vs-type-space.ts
+++ 007-value-space-vs-type-space.ts
@@ -3,21 +3,28 @@ type CreditCard = {
   cardholder: string;
   expirationDate: Date;
   secutiryCode: number;
-}
+};
 
 type DebitCard = {
   number: number;
   cardholder: string;
   expirationDate: Date;
   secutiryCode: number;
-}
+};
 
 type PaymentMethod = CreditCard | DebitCard;
 
 const purchase = (paymentMethod: PaymentMethod) => {
-  if (paymentMethod instanceof CreditCard) {
+  if (paymentMethod instanceof CreditCard) { // 'CreditCard' only refers to a type, but is being used as a value here.
     // purchase with credit card
   } else {
     // purchase with debit card
   }
 }
+
+const creditCard: CreditCard = {
+  number: 2093,
+  cardholder: 'TK',
+  expirationDate: new Date(),
+  secutiryCode: 101
+};

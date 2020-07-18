1765c8d1e3ca89568a9796386c50c5067b5e5629
TK <leandrotk100@gmail.com>
2020-07-18 19:02:40 -0300

    015: Intersection type

---
+++ 015-intersection-type.ts
@@ -0,0 +1,27 @@
+type ImageUrl = {
+  imageUrl: string;
+};
+
+type ImageAttr = {
+  imageAlt: string;
+  width?: string
+};
+
+type ImageProps = ImageUrl & ImageAttr;
+
+type ImageProps = {
+  imageUrl: string;
+  imageAlt: string;
+  width?: string
+};
+
+const imageProps: ImageProps = {
+  imageUrl: 'www.image.com',
+  imageAlt: 'an image',
+};
+
+const imageProps: ImageProps = {
+  imageUrl: 'www.image.com',
+  imageAlt: 'an image',
+  width: '100%'
+};

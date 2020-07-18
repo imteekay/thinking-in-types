e5d16d54bb26b345618a48316e730a7588815e5a
TK <leandrotk100@gmail.com>
2020-07-18 18:44:22 -0300

    Harry Potter in union type

--- 014-union-type.ts
+++ 014-union-type.ts
@@ -29,3 +29,78 @@ function reducer(state, action: Actions) {
     // submiting action
   }
 }
+
+// -------------------------------------------------------
+
+type House = {
+  name: string;
+  traits: string[];
+}
+
+const gryffindor: House = {
+  name: 'Gryffindor',
+  traits: ['courage', 'bravery']
+};
+
+const slytherin: House = {
+  name: 'Slytherin',
+  traits: ['ambition', 'leadership']
+};
+
+const ravenclaw: House = {
+  name: 'Ravenclaw',
+  traits: ['intelligence', 'learning']
+};
+
+const hufflepuff: House = {
+  name: 'Hufflepuff',
+  traits: ['hard work', 'patience']
+};
+
+const houses: House[] = [
+  gryffindor,
+  slytherin,
+  ravenclaw,
+  hufflepuff
+];
+
+type Witch = {
+  name: string;
+  trait: string;
+  magicFamily: string;
+}
+
+type Muggle = {
+  name: string;
+  trait: string;
+  email: string;
+}
+
+type Person = Muggle | Witch;
+
+const hermione: Muggle = {
+  name: 'Hermione Granger',
+  trait: 'bravery',
+  email: 'hermione@mail.com'
+};
+
+const harry: Witch = {
+  name: 'Harry Potter',
+  trait: 'courage',
+  magicFamily: 'Potter'
+};
+
+const tk: Person = {
+  name: 'TK',
+  email: 'tk@mail.com',
+  trait: 'learning',
+  magicFamily: 'Kinoshita'
+};
+
+function chooseHouse(houses: House[], person: Person) {
+  return houses.find((house) => house.traits.includes(person.trait))
+}
+
+chooseHouse(houses, harry); // { name: 'Gryffindor', traits: ['courage', 'bravery'] }
+chooseHouse(houses, hermione); // { name: 'Gryffindor', traits: ['courage', 'bravery'] }
+chooseHouse(houses, tk); // { name: 'Ravenclaw', traits: ['intelligence', 'learning'] }

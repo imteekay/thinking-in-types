f1baf9d7095f0fe1b582da60d9d4683681b35f68
TK <leandrotk100@gmail.com>
2020-08-18 09:50:01 -0300

    More on primitives

--- 003-primitive.ts
+++ 003-primitive.ts
@@ -1,3 +1,15 @@
 const isTypescript: boolean = true;
 const age: number = 24;
 const username: string = 'tk';
+
+// Can't assign different types
+const name: string = 2; // Type '2' is not assignable to type 'string'.
+const number: number = 'name' // Type '"name"' is not assignable to type 'number'.
+
+// Can't use different types for math operations
+'2' * '2';
+// The left-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.
+// The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type
+
+1 / '1';
+// The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type
